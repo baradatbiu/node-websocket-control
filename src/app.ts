ddcd2a11a8a1d0a9ea99dff7b37d4f03cc6e72a7
Dmitry Kharlamov <newdimaak@gmail.com>
2022-06-24 01:34:41 +0300

    feat: implement draw figures commands

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import Jimp from "jimp";
-import { httpServer } from "./http_server/index";
-import robot from "robotjs";
-import { WebSocketServer } from "ws";
 import "./env";
+import { httpServer } from "./http_server/index";
+import { createWebSocketStream, WebSocketServer } from "ws";
+import robot from "robotjs";
+// import Jimp from "jimp";
 
 const HTTP_PORT = Number(process.env.HTTP_PORT) || 3000;
 const WS_PORT = Number(process.env.WS_PORT) || 8080;
+const MOUSE_BUTTON = process.env.MOUSE_BUTTON || "left";
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
@@ -13,12 +14,19 @@ httpServer.listen(HTTP_PORT);
 const wss = new WebSocketServer({ port: WS_PORT });
 
 wss.on("connection", (ws) => {
-  ws.on("message", (data) => {
-    const [command, value] = data.toString().split(" ");
-    const numberOfPx = Number(value);
+  const duplex = createWebSocketStream(ws, {
+    encoding: "utf8",
+    decodeStrings: false,
+  });
+
+  duplex.on("data", (data: string) => {
+    const [command, valueOne, valueTwo = ""] = data.toString().split(" ");
+    const numberOfPx = parseInt(valueOne);
+
     console.log("received: %s", data);
+
     const { x, y } = robot.getMousePos();
-    const sendDefaultResult = () => ws.send(command);
+    const sendDefaultResult = () => duplex.write(command);
 
     switch (command) {
       case "mouse_up":
@@ -38,14 +46,65 @@ wss.on("connection", (ws) => {
         sendDefaultResult();
         break;
       case "mouse_position":
-        ws.send(`${command} ${x},${y}`);
+        duplex.write(`${command} ${x},${y}`);
         break;
+      case "draw_circle": {
+        let step = 0.01 * Math.PI;
+        const centerX = x - numberOfPx;
+        const centerY = y;
+        let nextX = 0;
+        let nextY = 0;
+
+        robot.mouseToggle("down", MOUSE_BUTTON);
+
+        for (let a = 0; a < 2 * Math.PI; a += step) {
+          nextX = centerX + numberOfPx * Math.cos(a);
+          nextY = centerY + numberOfPx * Math.sin(a);
+
+          robot.moveMouseSmooth(nextX, nextY);
+        }
+
+        robot.mouseToggle("up", MOUSE_BUTTON);
+
+        sendDefaultResult();
+        break;
+      }
+      case "draw_rectangle": {
+        const additionalNumberOfPx = parseInt(valueTwo);
+
+        const width = x + numberOfPx;
+        const height = y + additionalNumberOfPx;
+
+        robot.mouseToggle("down", MOUSE_BUTTON);
+        robot.moveMouseSmooth(width, y);
+        robot.moveMouseSmooth(width, height);
+        robot.moveMouseSmooth(x, height);
+        robot.moveMouseSmooth(x, y);
+        robot.mouseToggle("up", MOUSE_BUTTON);
+
+        sendDefaultResult();
+        break;
+      }
+      case "draw_square": {
+        const width = x + numberOfPx;
+        const height = y + numberOfPx;
+
+        robot.mouseToggle("down", MOUSE_BUTTON);
+        robot.moveMouseSmooth(width, y);
+        robot.moveMouseSmooth(width, height);
+        robot.moveMouseSmooth(x, height);
+        robot.moveMouseSmooth(x, y);
+        robot.mouseToggle("up", MOUSE_BUTTON);
+
+        sendDefaultResult();
+        break;
+      }
       default:
         break;
     }
 
-    console.log(`${command} x:${x},y:${y}\0`);
+    console.log(`${command} x:${x},y:${y}`);
   });
 
-  ws.send("something");
+  duplex.write("something");
 });
