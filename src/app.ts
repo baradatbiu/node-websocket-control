693166fc334062a0396c088eadac957a47ec6687
Dmitry Kharlamov <newdimaak@gmail.com>
2022-06-25 02:46:01 +0300

    refactor: app with commander, types

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import "./env";
 import { httpServer } from "./http_server/index";
 import { createWebSocketStream, WebSocketServer } from "ws";
-import robot from "robotjs";
-// import Jimp from "jimp";
+import { COMMANDS } from "./types/commands";
+import { Commander } from "./Commander/Commander";
 
 const HTTP_PORT = Number(process.env.HTTP_PORT) || 3000;
 const WS_PORT = Number(process.env.WS_PORT) || 8080;
@@ -13,98 +13,73 @@ httpServer.listen(HTTP_PORT);
 
 const wss = new WebSocketServer({ port: WS_PORT });
 
+const commander = new Commander(MOUSE_BUTTON);
+
 wss.on("connection", (ws) => {
+  console.log(`Start websocket server on the ${WS_PORT} port!`);
+
   const duplex = createWebSocketStream(ws, {
     encoding: "utf8",
     decodeStrings: false,
   });
 
-  duplex.on("data", (data: string) => {
+  duplex.on("data", async (data: string) => {
     const [command, valueOne, valueTwo = ""] = data.toString().split(" ");
     const numberOfPx = parseInt(valueOne);
 
     console.log("received: %s", data);
 
-    const { x, y } = robot.getMousePos();
-    const sendDefaultResult = () => duplex.write(command);
+    const sendDefaultResult = () => duplex.write(`${command}\0`);
 
     switch (command) {
-      case "mouse_up":
-        robot.moveMouse(x, y - numberOfPx);
+      case COMMANDS.MOUSE_UP:
+        commander.moveMouseUp(numberOfPx);
         sendDefaultResult();
         break;
-      case "mouse_down":
-        robot.moveMouse(x, y + numberOfPx);
+      case COMMANDS.MOUSE_DOWN:
+        commander.moveMouseDown(numberOfPx);
         sendDefaultResult();
         break;
-      case "mouse_left":
-        robot.moveMouse(x - numberOfPx, y);
+      case COMMANDS.MOUSE_LEFT:
+        commander.moveMouseLeft(numberOfPx);
         sendDefaultResult();
         break;
-      case "mouse_right":
-        robot.moveMouse(x + numberOfPx, y);
+      case COMMANDS.MOUSE_RIGHT:
+        commander.moveMouseRight(numberOfPx);
         sendDefaultResult();
         break;
-      case "mouse_position":
-        duplex.write(`${command} ${x},${y}`);
+      case COMMANDS.MOUSE_POSITION:
+        const { x, y } = commander.currentCoords;
+        duplex.write(`${command} ${x},${y}\0`);
         break;
-      case "draw_circle": {
-        let step = 0.01 * Math.PI;
-        const centerX = x - numberOfPx;
-        const centerY = y;
-        let nextX = 0;
-        let nextY = 0;
-
-        robot.mouseToggle("down", MOUSE_BUTTON);
-
-        for (let a = 0; a < 2 * Math.PI; a += step) {
-          nextX = centerX + numberOfPx * Math.cos(a);
-          nextY = centerY + numberOfPx * Math.sin(a);
-
-          robot.moveMouseSmooth(nextX, nextY);
-        }
-
-        robot.mouseToggle("up", MOUSE_BUTTON);
-
+      case COMMANDS.DRAW_CIRCLE:
+        commander.drawCircle(numberOfPx);
         sendDefaultResult();
         break;
-      }
-      case "draw_rectangle": {
-        const additionalNumberOfPx = parseInt(valueTwo);
-
-        const width = x + numberOfPx;
-        const height = y + additionalNumberOfPx;
-
-        robot.mouseToggle("down", MOUSE_BUTTON);
-        robot.moveMouseSmooth(width, y);
-        robot.moveMouseSmooth(width, height);
-        robot.moveMouseSmooth(x, height);
-        robot.moveMouseSmooth(x, y);
-        robot.mouseToggle("up", MOUSE_BUTTON);
-
+      case COMMANDS.DRAW_RECTANGLE:
+        commander.drawRectangle(numberOfPx, parseInt(valueTwo));
         sendDefaultResult();
         break;
-      }
-      case "draw_square": {
-        const width = x + numberOfPx;
-        const height = y + numberOfPx;
-
-        robot.mouseToggle("down", MOUSE_BUTTON);
-        robot.moveMouseSmooth(width, y);
-        robot.moveMouseSmooth(width, height);
-        robot.moveMouseSmooth(x, height);
-        robot.moveMouseSmooth(x, y);
-        robot.mouseToggle("up", MOUSE_BUTTON);
-
+      case COMMANDS.DRAW_SQUARE:
+        commander.drawSquare(numberOfPx);
         sendDefaultResult();
         break;
-      }
+      case COMMANDS.PRNT_SCRN:
+        const base64String = await commander.printScreen();
+        duplex.write(`${command} ${base64String}\0`);
       default:
         break;
     }
 
-    console.log(`${command} x:${x},y:${y}`);
+    commander.displayCommandResult(command);
   });
 
-  duplex.write("something");
+  duplex.on("error", () => {
+    duplex.write("Some error");
+  });
+});
+
+process.on("SIGINT", () => {
+  wss.close();
+  process.exit();
 });
