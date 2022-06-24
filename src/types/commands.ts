0660f0a9413f5d34046516cc77f957bb4c405294
Dmitry Kharlamov <newdimaak@gmail.com>
2022-06-25 02:40:17 +0300

    feat: add types

---
+++ src/types/commands.ts
@@ -0,0 +1,16 @@
+export type MouseCoords = {
+  x: number;
+  y: number;
+};
+
+export enum COMMANDS {
+  MOUSE_UP = "mouse_up",
+  MOUSE_DOWN = "mouse_down",
+  MOUSE_LEFT = "mouse_left",
+  MOUSE_RIGHT = "mouse_right",
+  MOUSE_POSITION = "mouse_position",
+  DRAW_CIRCLE = "draw_circle",
+  DRAW_RECTANGLE = "draw_rectangle",
+  DRAW_SQUARE = "draw_square",
+  PRNT_SCRN = "prnt_scrn",
+}
